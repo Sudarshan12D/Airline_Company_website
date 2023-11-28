47beea0f77f262b28a3b647c715da45efe983dd2
TurtwigPlayz <Nelsonthompson2003@gmail.com>
2023-11-28 10:31:30 -0700

    DatabaseConnection is now a singleton

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -3,20 +3,41 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
+    private static Connection connection;
+
+    private DatabaseConnection() {
+        // private constructor to prevent instantiation
+    }
+
     public static Connection getConnection() {
+        if (connection == null || isConnectionClosed()) {
+            initializeConnection();
+        }
+        return connection;
+    }
+
+    private static boolean isConnectionClosed() {
         try {
-            String url = "jdbc:mysql://localhost:3306/FLIGHTBOOKINGS";
+            return connection == null || connection.isClosed();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return true;
+        }
+    }
+
+    private static void initializeConnection() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/FLIGHT_BOOKINGS";
             String username = "root";
-            String password = "Barcelona15";
+            String password = "hgrt%48K";
 
             // Load the JDBC driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
+           Class.forName("com.mysql.cj.jdbc.Driver");
 
             // Establish a connection
-            return DriverManager.getConnection(url, username, password);
+            connection = DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
-            return null;
         }
     }
 }
