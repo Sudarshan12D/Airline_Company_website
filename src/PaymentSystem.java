bf5f83f3a2ed1429f7d56bc8a92de2320791167f
evanc38 <evan.cherewko@gmail.com>
2023-11-28 09:49:53 -0700

    payment system singelton

--- src/PaymentSystem.java
+++ src/PaymentSystem.java
@@ -1,10 +1,19 @@
 
 public class PaymentSystem {
     //Variables
-    
+    static PaymentSystem obj;
 
     //Member Functions
+    private PaymentSystem(){
 
+    }
+
+    public PaymentSystem getPaymentSystem(){
+        if (obj == null){
+            obj = new PaymentSystem();
+        }
+        return obj;
+    }
 
     //Getters
 
--- src/runProgram.py
+++ src/runProgram.py
@@ -4,5 +4,7 @@ import os
 
 
 #add run commands to easily compile and run.
+#os.system("javac -cp .;lib/mysql-connector-java-8.0.23/mysql-connector-java-8.0.23.jar edu/ucalgary/oop/Animal.java edu/ucalgary/oop/Clinic.java edu/ucalgary/oop/SpeciesData.java edu/ucalgary/oop/SQLData.java edu/ucalgary/oop/Task.java edu/ucalgary/oop/TimeSlot.java edu/ucalgary/oop/Treatment.java edu/ucalgary/oop/gui.java")
+#os.system("java -cp .;lib/mysql-connector-java-8.0.23/mysql-connector-java-8.0.23.jar edu/ucalgary/oop/gui")
 os.system("javac *.java")
 os.system("java MainTestFile")
