cea661b60d18f48d88e6ab1bece5cb6c1a4d9cdb
evanc38 <evan.cherewko@gmail.com>
2023-11-28 13:36:58 -0700

    removed redundant function

--- FlightDataRetriever.java
+++ FlightDataRetriever.java
@@ -119,32 +119,4 @@ public class FlightDataRetriever {
         //return flight list
         return fl;
     }
-
-
-
-
-
-
-    public static ArrayList<Object[]> getAvailableFlights() {
-        ArrayList<Object[]> flights = new ArrayList<>();
-        String sql = "SELECT FlightID, Origin, Destination, DepartureDateTime, ArrivalDateTime FROM Flights";
-
-        try (Connection conn = DatabaseConnection.getConnection();
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
-
-            while (rs.next()) {
-                Object[] row = new Object[5]; // 5 columns in the flight table
-                row[0] = rs.getInt("FlightID");
-                row[1] = rs.getString("Origin");
-                row[2] = rs.getString("Destination");
-                row[3] = rs.getTimestamp("DepartureDateTime");
-                row[4] = rs.getTimestamp("ArrivalDateTime");
-                flights.add(row);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return flights;
-    }
 }
