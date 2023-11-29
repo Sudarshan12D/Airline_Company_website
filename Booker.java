ccfeee4ee6f064a94e9ff6dddff82b3f9f403b7a
TurtwigPlayz <Nelsonthompson2003@gmail.com>
2023-11-29 15:14:59 -0700

    Booker Update

--- Booker.java
+++ Booker.java
@@ -42,7 +42,37 @@ public class Booker {
         }
         return id;
     }
+    public static long addPayment(int memberID, int flightID, int seatID, Boolean insurance) {
+        String SQL = "INSERT INTO Bookings(UserID, FlightID, SeatID, CancelationInsurance) VALUES (?, ?, ?, ?)";
 
+        long id = 0;
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS)) {
+
+            pstmt.setInt(1, memberID);
+            pstmt.setInt(2, flightID);
+            pstmt.setInt(3, seatID);
+            pstmt.setBoolean(4, insurance);
+
+            int affectedRows = pstmt.executeUpdate();
+
+            // Check the affected rows
+            if (affectedRows > 0) {
+                // Get the ID back
+                try (ResultSet rs = pstmt.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        id = rs.getLong(1);
+                    }
+                } catch (SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return id;
+    }
     
 
 }
